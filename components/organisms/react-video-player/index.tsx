842030c535585fcf4d6bb106f2ece14d1999257e
vavgithub <helloatvav@gmail.com>
2025-11-27 13:21:40 +0530

    intital v7

--- components/organisms/react-video-player/index.tsx
+++ components/organisms/react-video-player/index.tsx
@@ -5,7 +5,7 @@ import ReactPlayer from "react-player";
 
 interface ReactVideoPlayerProps {
   className?: string;
-  videoUrl: string;
+  videoUrl: string; // HLS (.m3u8) or MP4 URL
   loop?: boolean;
   showControls?: boolean;
 }
@@ -16,68 +16,70 @@ export const ReactVideoPlayer = ({
   loop = true,
   showControls = false,
 }: ReactVideoPlayerProps) => {
-  const playerRef = useRef(null);
-  const [muted, setMuted] = useState(true);
+  const ref = useRef(null);
+  const [muted, setMuted] = useState<boolean>(true);
 
   return (
-    <div className={`relative w-full h-full ${className}`}>
-      
-      {/* MUTE BUTTON (TOP LAYER) */}
+    <div className={`relative  w-full h-full ${className}`}>
       <div
-        onClick={() => setMuted(!muted)}
-        className="absolute top-8 left-8 z-[9999] cursor-pointer p-2 bg-black/40 rounded-full"
+        onClick={(e) => {
+            e.stopPropagation();    
+            setMuted(!muted)
+        }}
+        className={`fixed sm:absolute bg-black/40 p-3 rounded-full transition-opacity duration-100 ease-linear cursor-pointer sm:top-6 right-8 sm:right-auto sm:bottom-auto bottom-8 sm:left-6 flex items-center justify-center z-50`}
       >
-        <div className="text-white">
-          {!muted ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z" />
-              <path d="M16 9a5 5 0 0 1 0 6" />
-              <path d="M19.364 18.364a9 9 0 0 0 0-12.728" />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M16 9a5 5 0 0 1 .95 2.293" />
-              <path d="M19.364 5.636a9 9 0 0 1 1.889 9.96" />
-              <path d="m2 2 20 20" />
-              <path d="m7 7-.587.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298V11" />
-              <path d="M9.828 4.172A.686.686 0 0 1 11 4.657v.686" />
-            </svg>
-          )}
+        <div className="flex flex-col gap-4 md:gap-12 justify-center items-center ">
+          <div className="text-white ">
+            {!muted ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-volume2-icon lucide-volume-2"
+              >
+                <path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z" />
+                <path d="M16 9a5 5 0 0 1 0 6" />
+                <path d="M19.364 18.364a9 9 0 0 0 0-12.728" />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-volume-off-icon lucide-volume-off"
+              >
+                <path d="M16 9a5 5 0 0 1 .95 2.293" />
+                <path d="M19.364 5.636a9 9 0 0 1 1.889 9.96" />
+                <path d="m2 2 20 20" />
+                <path d="m7 7-.587.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298V11" />
+                <path d="M9.828 4.172A.686.686 0 0 1 11 4.657v.686" />
+              </svg>
+            )}
+          </div>
         </div>
       </div>
-
-      {/* VIDEO (BOTTOM LAYER) */}
-      <div className="absolute inset-0 z-[1]">
+      <div className="pointer-events-none video-no-events">
         <ReactPlayer
-          ref={playerRef}
-          src={videoUrl}
-          playing={true}
-          muted={muted}
-          controls={showControls}
-          loop={loop}
-          width="100%"
-          height="100%"
-          playsInline
+            ref={ref}
+            src={videoUrl}
+            playing={true}
+            muted={muted}
+            controls={showControls}
+            loop={loop}
+            width="100%"
+            height="100%"
         />
       </div>
     </div>
